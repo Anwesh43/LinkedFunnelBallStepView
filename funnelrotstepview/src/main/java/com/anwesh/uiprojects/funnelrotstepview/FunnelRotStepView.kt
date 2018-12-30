852166508a9fd738871e465acde6ccbb1cd7b92d
Anwesh43 <anweshthecool0@gmail.com>
2018-12-30 11:43:37 +0530

    adding linbribraries and defining constants

--- funnelrotstepview/src/main/java/com/anwesh/uiprojects/funnelrotstepview/FunnelRotStepView.kt
+++ funnelrotstepview/src/main/java/com/anwesh/uiprojects/funnelrotstepview/FunnelRotStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.funnelrotstepview
 /**
  * Created by anweshmishra on 30/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.7f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#2980b9")
+val backColor : Int = Color.parseColor("#BDBDBD")
