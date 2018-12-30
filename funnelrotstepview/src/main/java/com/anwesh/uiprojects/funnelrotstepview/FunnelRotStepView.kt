fe30e9986051381853cd50e5a898850366632c63
Anwesh43 <anweshthecool0@gmail.com>
2018-12-30 12:10:41 +0530

    adding FunnelRotStep class

--- funnelrotstepview/src/main/java/com/anwesh/uiprojects/funnelrotstepview/FunnelRotStepView.kt
+++ funnelrotstepview/src/main/java/com/anwesh/uiprojects/funnelrotstepview/FunnelRotStepView.kt
@@ -165,4 +165,27 @@ class FunnelRotStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class FunnelRotStep(var i : Int) {
+
+        private val root : FRSNode = FRSNode(0)
+        private var curr : FRSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
