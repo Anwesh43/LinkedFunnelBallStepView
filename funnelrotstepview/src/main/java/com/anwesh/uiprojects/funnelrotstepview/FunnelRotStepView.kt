a559be2fc2d487866a9715a12b5ddf2cb0301125
Anwesh43 <anweshthecool0@gmail.com>
2018-12-30 11:49:25 +0530

    adding Float and Int extension methods

--- funnelrotstepview/src/main/java/com/anwesh/uiprojects/funnelrotstepview/FunnelRotStepView.kt
+++ funnelrotstepview/src/main/java/com/anwesh/uiprojects/funnelrotstepview/FunnelRotStepView.kt
@@ -21,6 +21,13 @@ val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#2980b9")
 val backColor : Int = Color.parseColor("#BDBDBD")
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse()).toFloat()
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this/scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float = ((1 - scaleFactor()) * a.inverse()) + (scaleFactor() * b.inverse())
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
+
 class FunnelRotStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
